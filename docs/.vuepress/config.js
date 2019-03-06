b68ecbada54f3de7563a44cc3b9f06389affb0a5
TakumaNakagame <service.kame@gmail.com>
2019-03-06 20:34:14 +0900

    add Github Link

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,6 +8,7 @@ module.exports = {
       { text: 'FreeNASについて', link: '/Introduction/'},
       { text: 'インストール', link: '/Install/'},
       { text: 'English/Official Site', link: 'https://www.ixsystems.com/documentation/freenas/'},
+      { text: 'GitHub', link: 'https://github.com/TakumaNakagame/freenas-doc-ja'},
     ],
     sidebar: 'auto',
     sidebarDepth: 3,
